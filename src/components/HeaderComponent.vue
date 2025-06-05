f12605984395ac98ee446a31d2777183557f1b27
Max Bokhan <bokhanworks@gmail.com>
2025-06-05 14:47:57 +0300

    routerlink fix

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -31,7 +31,7 @@
 
                 <!-- Меню -->
                 <nav class="flex flex-wrap justify-center md:justify-end gap-6">
-                    <a href="/internet" class="text-green-dark hover:text-green-500 transition-colors">Інтернет</a>
+                    <RouterLink to="/internet" class="text-green-dark hover:text-green-500 transition-colors">Інтернет</RouterLink>
                     <!--<a href="/utilities" class="text-green-dark hover:text-green-500 transition-colors">Система</a>
                     <a href="/media" class="text-green-dark hover:text-green-500 transition-colors">Медіа</a>
                     <a href="/development" class="text-green-dark hover:text-green-500 transition-colors">Розробка</a>
