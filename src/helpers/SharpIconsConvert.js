44be1fb9d152b09ef31fa305cb8bf28c7a345fd2
Max Bokhan <bokhanworks@gmail.com>
2026-07-03 13:54:01 +0300

    Add WebP format to icons convert script

--- src/helpers/SharpIconsConvert.js
+++ src/helpers/SharpIconsConvert.js
@@ -16,8 +16,8 @@ async function convertIcons() {
       return
     }
 
-    // Get all PNG, JPG, SVG files from source directory
-    const files = fs.readdirSync(sourceDir).filter((file) => /\.(png|jpe?g|svg)$/i.test(file))
+    // Get all PNG, JPG, SVG, WebP files from source directory
+    const files = fs.readdirSync(sourceDir).filter((file) => /\.(png|jpe?g|svg|webp)$/i.test(file))
 
     if (files.length === 0) {
       console.log('⚠️  В директорії source не знайдено PNG файлів')
